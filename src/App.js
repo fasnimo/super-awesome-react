1e0df6afc1661afd07ad9f9ea0ba0145c2b6c559
fasnimo <fasnimo@gmail.com>
2021-03-08 21:34:48 -0600

    router created and react router installed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeroContainer from "./components/HeroContainer"
 
 const App = () => {
   return (
---
+++ src/components/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Router = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Router;
